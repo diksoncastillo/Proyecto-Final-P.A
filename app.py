e444b9dd5a9fa1bb20f70e1a5baeb12f04afd8cd
diksoncastillo <159024721+diksoncastillo@users.noreply.github.com>
2025-07-10 03:30:06 -0500

    Subiendo Archivos
    Muejeje

---
+++ app.py
@@ -0,0 +1,35 @@
+# https://youtu.be/bluclMxiUkA
+"""
+Application that predicts heart disease percentage in the population of a town
+based on the number of bikers and smokers. 
+
+Trained on the data set of percentage of people biking 
+to work each day, the percentage of people smoking, and the percentage of 
+people with heart disease in an imaginary sample of 500 towns.
+
+"""
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+ 
+app = Flask(__name__)
+
+model = pickle.load(open('models/model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
+    features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
+    prediction = model.predict(features)  # features Must be in the form [[a, b]]
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
+
+if __name__ == '__main__':
+    app.run(debug=True)
